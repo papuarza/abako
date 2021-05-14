035dd80e6f78c294a624fc7ee8b472e2729a665e
Victor Arza <papu.arza@gmail.com>
2021-05-14 16:49:22 -0400

    da

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-const sslRedirect  = require('heroku-ssl-redirect);
+const sslRedirect  = require('heroku-ssl-redirect');
 
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
